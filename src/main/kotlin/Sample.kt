616744534ab20c46c273a0b99e360c1190c0e67a
prafullKrRj <prafullkumar384@gmail.com>
2024-05-11 12:32:53 +0530

    Test

--- src/main/kotlin/Sample.kt
+++ src/main/kotlin/Sample.kt
@@ -4,3 +4,11 @@ class Sample {
     private val name = "Prafull"
     fun getName() = name
 }
+class LLMClient {
+    companion object {
+        fun getName() : String {
+            val sample = Sample()
+            return sample.getName()
+        }
+    }
+}
